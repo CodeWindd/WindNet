03ae7f3255f5c0ce56dff524a59b9b0b04a5fe08
CodeWindd <djvorttoriginal@outlook.com>
2026-06-07 06:38:03 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
 /* ==========================================================================
    WINDYWEATHER OFFLINE SERVICE WORKER
-   Enables offline caching for progressive web app systems
    ========================================================================== */
 
-const CACHE_NAME = 'windyweather-pwa-cache-v3';
+const CACHE_NAME = 'windyweather-pixel-cache-v1';
 const CORE_ASSETS = [
     './',
     './index.html',
